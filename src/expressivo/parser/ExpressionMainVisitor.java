78aab5556c58d42bb60b3c6e0b29360807056367
dawiditer <dawiditer@gmail.com>
2017-12-11 18:12:44 +0300

    Modifies default visitors to return expressions

--- src/expressivo/parser/ExpressionMainVisitor.java
+++ src/expressivo/parser/ExpressionMainVisitor.java
@@ -7,6 +7,14 @@ import expressivo.Value;
 import expressivo.Variable;
 
 public class ExpressionMainVisitor extends ExpressionBaseVisitor<Expression> {
+    /** Returns the whole expression generated from the parse tree */
+    @Override public Expression visitRoot(ExpressionParser.RootContext ctx) { 
+        return visit(ctx.stat()); 
+    }
+    /** Returns the whole expression generated from the parse tree */
+    @Override public Expression visitStat(ExpressionParser.StatContext ctx) { 
+        return visit(ctx.expr()); 
+    }
     /** Creates an addition expression on every visit */
     @Override public Expression visitAdd(ExpressionParser.AddContext ctx) {
         Expression leftExpr = visit(ctx.expr(0));
@@ -14,7 +22,10 @@ public class ExpressionMainVisitor extends ExpressionBaseVisitor<Expression> {
         
         return new Addition(leftExpr, rightExpr);
     }
-
+    /** Returns the expression inside braces */
+    @Override public Expression visitBrackets(ExpressionParser.BracketsContext ctx) {
+        return visit(ctx.expr()); 
+    }
     /** Creates a multiplication expression on every visit */
     @Override public Expression visitMult(ExpressionParser.MultContext ctx) {
         Expression leftExpr = visit(ctx.expr(0));
